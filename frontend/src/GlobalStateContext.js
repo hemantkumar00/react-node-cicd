8f075ec7035b7beb52a97993d48aa370678cb4b8
hemantkumar00 <hemantkumar23090@gmail.com>
2023-10-22 18:28:55 +0530

    node-react CI-CD 1

--- frontend/src/GlobalStateContext.js
+++ frontend/src/GlobalStateContext.js
@@ -8,7 +8,6 @@ import React, {
 } from "react";
 import { toast } from "react-toastify";
 import LoadingSpinner from "./LoadingSpinner";
-import { API } from "./API";
 
 const GlobalStateContext = createContext();
 
