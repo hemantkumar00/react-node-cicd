819041c2f7e57afd6afbb56c021148f7c429db91
hemantkumar00 <hemantkumar23090@gmail.com>
2023-10-22 17:31:26 +0530

    node-react CI-CD 1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+// import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:8000", // Adjust the URL based on your backend API URL
-});
+// const api = axios.create({
+//   baseURL: "http://localhost:8000", // Adjust the URL based on your backend API URL
+// });
 
-export default api;
+// export default api;
