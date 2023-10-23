20973a17d9cc742f90462e6393b7a08fe8daf7c2
hemantkumar00 <hemantkumar23090@gmail.com>
2023-10-23 12:26:39 +0530

    node-react CI-CD 1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build/index.html")));
 
 mongoose
   .connect(
